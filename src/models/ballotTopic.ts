830b993359c1d8c637f34afa7ab1d7524756f4b5
Kayoung <rkdkdudjd@gmail.com>
2022-07-09 12:44:57 +0900

    feat: 투표 주제 스키마

---
+++ src/models/ballotTopic.ts
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+import IDocument from "./interface/Document";
+import IBallotTopic from "./interface/IBallotTopic";
+
+type BallotTopicDocument = IBallotTopic & IDocument;
+
+const ballotTopicSchema = new Schema<BallotTopicDocument>({
+  topic: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const BallotTopic = model<BallotTopicDocument>('BallotTopic', ballotTopicSchema);
+
+export default BallotTopic;
