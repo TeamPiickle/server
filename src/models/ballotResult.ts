95a9be14acea3fa49ad710f23aff348f9f5a84da
Kayoung <rkdkdudjd@gmail.com>
2022-07-09 12:45:17 +0900

    feat: 투표 결과 스키마

---
+++ src/models/ballotResult.ts
@@ -0,0 +1,35 @@
+import { Types, Schema, model } from "mongoose";
+import IDocument from "./interface/Document";
+import IBallotResult from "./interface/IBallotResult";
+
+type BallotResultDocument = IBallotResult & IDocument;
+
+const ballotResultSchema = new Schema<BallotResultDocument>({
+  ballotTopicId: {
+    type: Schema.Types.ObjectId,
+    ref: 'BallotTopic',
+    required: true,
+  },
+  ballotItemId: {
+    type: Schema.Types.ObjectId,
+    ref: 'BallotItem',
+    required: true,
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    requried: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+})
+
+const BallotResult = model<BallotResultDocument>('BallotResult', ballotResultSchema);
+
+export { IBallotResult, BallotResult }
