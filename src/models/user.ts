b99934bc56b0d2dbc78b04b686e8f0f1dc5720fa
Kayoung <rkdkdudjd@gmail.com>
2022-07-09 12:44:44 +0900

    feat: User 스키마

---
+++ src/models/user.ts
@@ -0,0 +1,36 @@
+import IDocument from "./interface/Document";
+import IUser from "./interface/IUser";
+import { model, Schema } from 'mongoose';
+
+type UserDocument = IUser & IDocument;
+
+const userSchema = new Schema<UserDocument>({
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  nickname: {
+    type: String,
+    required: true,
+  },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+  profileImageUrl: {
+    type: String,
+    required: false,
+  },
+  bookmarks: [{
+    type: Schema.Types.ObjectId,
+    required: false,
+  }]
+})
+
+const User = model<UserDocument>('User', userSchema);
+
+export default User;
