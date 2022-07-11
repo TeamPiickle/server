8269dab4e74038d122aebce94b3aeb0127c9b10e
Kayoung <rkdkdudjd@gmail.com>
2022-07-11 12:08:15 +0900

    fix: active timestamp

--- src/models/ballotResult.ts
+++ src/models/ballotResult.ts
@@ -4,31 +4,28 @@ import IBallotResult from './interface/IBallotResult';
 
 type BallotResultDocument = IBallotResult & IDocument;
 
-const ballotResultSchema = new Schema<BallotResultDocument>({
-  ballotTopicId: {
-    type: Schema.Types.ObjectId,
-    ref: 'BallotTopic',
-    required: true
+const ballotResultSchema = new Schema<BallotResultDocument>(
+  {
+    ballotTopicId: {
+      type: Schema.Types.ObjectId,
+      ref: 'BallotTopic',
+      required: true
+    },
+    ballotItemId: {
+      type: Schema.Types.ObjectId,
+      ref: 'BallotItem',
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      requried: true
+    }
   },
-  ballotItemId: {
-    type: Schema.Types.ObjectId,
-    ref: 'BallotItem',
-    required: true
-  },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    requried: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true
   }
-});
+);
 
 const BallotResult = model<BallotResultDocument>(
   'BallotResult',
