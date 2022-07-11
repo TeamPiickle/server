019bd734c48ddea0b524b246225d859b7467da29
donglee99 <jhnj741@naver.com>
2022-07-11 15:24:24 +0900

    [FEAT] 로그인 API

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,6 +2,10 @@ import { NextFunction, Response } from 'express';
 import { validationResult } from 'express-validator';
 import Request, { IllegalArgumentException } from '../intefaces/common';
 import CreateUserCommand from '../intefaces/createUserCommand';
+import { PostBaseResponseDto } from '../intefaces/PostBaseResponseDto';
+import { UserLoginDto } from '../intefaces/UserLoginDto';
+import User from '../models/user';
+import getToken from '../modules/jwtHandler';
 import message from '../modules/responseMessage';
 import statusCode from '../modules/statusCode';
 import util from '../modules/util';
@@ -32,4 +36,32 @@ const postUser = async (
   }
 };
 
-export default { postUser };
+/**
+ *  @route /users/login
+ *  @desc 로그인 api
+ *  @access Public
+ */
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+  const error = validationResult(req);
+
+  if (!error.isEmpty) {
+    throw new IllegalArgumentException('필요한 값이 없습니다.');
+  }
+
+  const userLoginDto: UserLoginDto = req.body;
+  try {
+    const result = await UserService.loginUser(userLoginDto);
+    const accessToken = getToken((result as PostBaseResponseDto)._id);
+    const data = {
+      _id: (result as PostBaseResponseDto)._id,
+      accessToken
+    };
+    return res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, message.USER_LOGIN_SUCCESS, data));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export default { postUser, loginUser };
