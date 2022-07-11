84dce5bb25f5788a4d08f88eac7b69c3928bb9c8
Kayoung <rkdkdudjd@gmail.com>
2022-07-11 20:14:29 +0900

    [FIX] 투표 주제 id 검증

--- src/services/ballotService.ts
+++ src/services/ballotService.ts
@@ -2,9 +2,15 @@ import { IllegalArgumentException, IllegalStateException } from '../intefaces/co
 import CreateBallotResultDto from '../intefaces/CreateBallotResultDto';
 import BallotItem from '../models/ballotItem';
 import { BallotResult } from '../models/ballotResult';
+import BallotTopic from '../models/ballotTopic';
 
 
 const createBallotResult = async (command: CreateBallotResultDto) => {
+  const ballotTopic = await BallotTopic.findById(command.ballotTopicId)
+  if (!ballotTopic) {
+    throw new IllegalArgumentException('올바르지 않은 투표 주제 id 입니다.');
+  }
+  
   const alreadyBallotResult = await BallotResult.findOne({
     ballotTopicId: command.ballotTopicId,
     userId: command.user.id
