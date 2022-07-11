dc1690fb5121f30f94b2be5ed69ab5598a3ca322
donglee99 <jhnj741@naver.com>
2022-07-11 15:31:27 +0900

    [FEAT] 로그인 Exception 핸들링

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -1,9 +1,11 @@
 const message = {
   USER_CREATED: '회원가입 성공',
   NULL_VALUE: '필요한 값이 없습니다.',
+  NULL_VALUE_TOKEN: '토큰이 유효하지 않습니다.',
   NOT_FOUND: '존재하지 않는 자원',
   BAD_REQUEST: '잘못된 요청',
-  INTERNAL_SERVER_ERROR: '서버 내부 오류'
+  INTERNAL_SERVER_ERROR: '서버 내부 오류',
+  USER_LOGIN_SUCCESS: '로그인 성공'
 };
 
 export default message;
