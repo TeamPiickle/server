954dd08483be53b8ec03895a7911092dd7654910
Kayoung <rkdkdudjd@gmail.com>
2022-07-11 12:46:56 +0900

    feat: piickle error

--- src/intefaces/common.ts
+++ src/intefaces/common.ts
@@ -4,3 +4,10 @@ export default interface Request<B = any, Q = any, P = any, C = any> {
   params: P;
   cookies: C;
 }
+
+export class PiickleError extends Error {
+  constructor(msg: string) {
+    super();
+    super.message = msg;
+  }
+}
