c79db3dedaf24ac822db4bb5dbecccd17cc7fa44
Kayoung <rkdkdudjd@gmail.com>
2022-07-13 21:42:12 +0900

    [CHORE] DTO에 id 필드 추가

--- src/intefaces/CardResponseDto.ts
+++ src/intefaces/CardResponseDto.ts
@@ -1,6 +1,7 @@
 import { Types } from 'mongoose';
 
 export interface CardResponseDto {
+  _id: Types.ObjectId;
   content: string;
   tags: string[];
   category: Types.ObjectId[];
--- src/intefaces/CategoryResponseDto.ts
+++ src/intefaces/CategoryResponseDto.ts
@@ -1,6 +1,8 @@
 import { CardResponseDto } from './CardResponseDto';
+import { Types } from 'mongoose';
 
 export default interface CategoryResponseDto {
+  _id: Types.ObjectId;
   title: string;
   cardList: CardResponseDto[];
 }
