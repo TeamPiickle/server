454767ddedf8787737d3e34b19c6dada6cb5bac4
Kayoung <rkdkdudjd@gmail.com>
2022-07-14 17:59:44 +0900

    [CHORE] 빌드 오류 수정

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -1,22 +1,27 @@
 import Bookmark from '../models/bookmark';
 import Card from '../models/card';
+import util from '../modules/util';
+import { Types } from 'mongoose';
+import { CardResponseDto } from '../intefaces/CardResponseDto';
+interface CardIdAndCnt {
+  _id: Types.ObjectId;
+  count: number;
+}
 
 const findBestCards = async (size: number) => {
-  const beforeAMonth = new Date();
-  beforeAMonth.setMonth(beforeAMonth.getMonth() - 1);
-
-  const cardIdAndCnt = await Bookmark.aggregate([
-    { $match: { createdAt: { $gte: beforeAMonth } } }
+  const cardIdAndCnt = <CardIdAndCnt[]>await Bookmark.aggregate([
+    { $match: { createdAt: { $gte: util.getLastMonth() } } }
   ])
     .sortByCount('card')
     .limit(size);
-  const cards = await Promise.all(
+
+  const cards = <CardResponseDto[]>await Promise.all(
     cardIdAndCnt.map(async c => {
       return Card.findById(c._id, '_id category content');
     })
   );
 
-  let extraCard;
+  let extraCard: CardResponseDto[] = [];
   if (cards.length < size) {
     extraCard = await Card.find({ _id: { $nin: cards.map(c => c._id) } }).limit(
       size - cards.length
