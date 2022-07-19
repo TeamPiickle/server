0f3ca6a61a95d50a69989b690c853c573abdde89
csb9427 <csb9427@naver.com>
2022-07-19 15:17:08 +0900

    [FIX] conflict 해결

--- src/services/CategoryService.ts
+++ src/services/CategoryService.ts
@@ -1,7 +1,10 @@
 import { Category, CategoryDocument } from '../models/category';
 import Card from '../models/card';
 import CategoryResponseDto from '../intefaces/CategoryResponseDto';
+import Types from 'mongoose';
 import { CardResponseDto } from '../intefaces/CardResponseDto';
+import Bookmark from '../models/bookmark';
+import { IllegalArgumentException } from '../intefaces/exception';
 
 const shuffleCategories = (arr: CategoryDocument[]) => {
   arr.sort(() => Math.random() - 0.5);
@@ -25,27 +28,77 @@ const getCategory = async (): Promise<Array<object> | null> => {
   return categories;
 };
 
-const getCards = async (
+const getCardsWithIsBookmark = async (
+  userId: Types.ObjectId | undefined,
   categoryId: string
 ): Promise<CategoryResponseDto | null> => {
-  const cards: CategoryResponseDto | null = await Category.findById(
-    categoryId
-  ).populate('cardIdList');
+  const cards = await Category.findById(categoryId)
+    .populate('cardIdList')
+    .then(item => {
+      if (!item) {
+        throw new IllegalArgumentException('해당 id의 카테고리가 없습니다.');
+      }
+      return {
+        _id: item._id,
+        title: item.title,
+        cardList: item.cardIdList
+      };
+    });
+
   if (!cards) return null;
-  return cards;
+  const cardList = await Promise.all(
+    cards.cardList.map(async (item: any) => {
+      const isBookmark =
+        (await Bookmark.find({ user: userId, card: item._id }).count()) > 0
+          ? true
+          : false;
+      return {
+        _id: item._id,
+        content: item.content,
+        tags: item.tags,
+        category: item.Category,
+        filter: item.filter,
+        isBookmark: isBookmark
+      };
+    })
+  );
+
+  return {
+    _id: cards._id,
+    title: cards.title,
+    cardList
+  };
 };
 
 const getCardsBySearch = async (
-  search: string[]
+  search: string[],
+  userId: Types.ObjectId | undefined
 ): Promise<CardResponseDto[] | null> => {
   try {
     const cards = await Card.find({ filter: { $all: search } });
     shuffleCards(cards);
-    return cards;
+    if (!cards) return null;
+    const cardList = await Promise.all(
+      cards.map(async (item: any) => {
+        const isBookmark =
+          (await Bookmark.find({ user: userId, card: item._id }).count()) > 0
+            ? true
+            : false;
+        return {
+          _id: item._id,
+          content: item.content,
+          tags: item.tags,
+          category: item.Category,
+          filter: item.filter,
+          isBookmark: isBookmark
+        };
+      })
+    );
+    return cardList;
   } catch (error) {
     console.log(error);
     throw error;
   }
 };
 
-export { getCategory, getCards, getCardsBySearch };
+export { getCategory, getCardsBySearch, getCardsWithIsBookmark };
