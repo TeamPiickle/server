b852ca478e7d10e602be2c2f6778050181daeb99
Kayoung Yoon <rkdkdudjd@gmail.com>
2022-09-15 01:22:22 +0900

    [FEAT] 개발용 회원 관련 api  (#127)
    * [FEAT] 회원 삭제
    
    * [FEAT] 인증 처리
    
    개발용

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,7 @@ import UserRouter from './userRouter';
 import CategoryRouter from './CategoryRouter';
 import BallotRouter from './ballotRouter';
 import CardRouter from './cardRouter';
+import TestRouter from './testRouter';
 
 const router = Router();
 
@@ -11,5 +12,6 @@ router.use('/users', UserRouter);
 router.use('/categories', CategoryRouter);
 router.use('/ballots', BallotRouter);
 router.use('/cards', CardRouter);
+router.use('/test', TestRouter);
 
 export default router;
---
+++ src/routes/testRouter.ts
@@ -0,0 +1,60 @@
+import { Router } from 'express';
+import User from '../models/user';
+import util from '../modules/util';
+import { TypedRequest } from '../types/TypedRequest';
+import config from '../config';
+import statusCode from '../modules/statusCode';
+import PreUser from '../models/preUser';
+
+const router = Router();
+
+router.delete(
+  '/user',
+  async (req: TypedRequest<{ email: string; key: string }>, res, next) => {
+    const { email, key } = req.body;
+    if (key !== config.clientKey) {
+      return res
+        .status(statusCode.FORBIDDEN)
+        .send(util.fail(statusCode.FORBIDDEN, '권한이 없습니다.'));
+    }
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).send(util.fail(400, '없는 이메일'));
+      }
+      await user.delete();
+      res
+        .status(200)
+        .send(util.success(200, '유저 삭제 성공', { deletedUserEmail: email }));
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+const DUMMY_PASSWORD = '123450qwertyu';
+
+router.post(
+  '/preuser',
+  async (req: TypedRequest<{ email: string; key: string }>, res, next) => {
+    const { email, key } = req.body;
+    if (key !== config.clientKey) {
+      return res
+        .status(statusCode.FORBIDDEN)
+        .send(util.fail(statusCode.FORBIDDEN, '권한이 없습니다.'));
+    }
+    try {
+      const preUser = new PreUser({
+        email,
+        password: DUMMY_PASSWORD,
+        emailVerified: true
+      });
+      await preUser.save();
+      res.status(200).json({ message: `${email} 인증 처리 성공` });
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+export default router;
