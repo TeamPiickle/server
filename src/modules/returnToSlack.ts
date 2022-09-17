f4c0d5522bfcc11021ba828eb4cf8cdf8004d25e
Kayoung Yoon <rkdkdudjd@gmail.com>
2022-09-17 16:41:44 +0900

    [FIX] 유저에이전트 추가 (#129)

--- src/modules/returnToSlack.ts
+++ src/modules/returnToSlack.ts
@@ -1,11 +1,16 @@
+import { Request } from 'express';
 export const slackMessage = (
-  method: string,
-  originalUrl: string,
+  req: Request,
   error: any,
   uid?: string,
   token?: string
 ): string => {
-  return `🚨  [ERROR] [${method}] \`${originalUrl}\`\n${
-    uid ? `user_id: ${uid}` : ''
-  }\n${token ? `token: ${token}` : ''}\n${JSON.stringify(error)}`;
+  const method = req.method.toUpperCase();
+  const originalUrl = req.originalUrl;
+  return `🚨  [ERROR] [${method}] \`${originalUrl}\`\n
+      Express-Host: ${req.header('Host') || 'Unknown'}\n
+      User-Agent: ${req.header('User-Agent') || 'Unknown'}\n
+  ${uid ? `      user_id: ${uid}\n` : ''}${
+    token ? `      token: ${token}\n` : ''
+  }      ${JSON.stringify(error)}`;
 };
