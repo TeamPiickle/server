873a03dbd8680235ed9947215119ea58e21708c8
Kayoung Yoon <dev.kayoung@gmail.com>
2023-01-22 17:23:46 +0900

    [FEAT] 카테고리 스키마에 그라데이션 필드 추가

--- src/models/interface/ICategory.ts
+++ src/models/interface/ICategory.ts
@@ -3,7 +3,7 @@ import { Types } from 'mongoose';
 export default interface ICategory {
   title: string;
   content: string;
-  imgurl: string;
   cardIdList: Types.ObjectId[];
-  unicode: string;
+  gradation: string;
+  order: number;
 }
