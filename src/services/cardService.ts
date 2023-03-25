4b7c2c6c05e71543145ad27da1ca776365f151e2
Rosie <dev.kayoung@gmail.com>
2023-03-26 03:20:48 +0900

    [FEAT] 베스트 피클 산출 방식 수정

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -5,86 +5,66 @@ import { Types } from 'mongoose';
 import { CardResponseDto } from '../intefaces/CardResponseDto';
 import BestCard from '../models/bestCard';
 import { Nullable } from '../types/types';
+import user from '../models/user';
 interface CardIdAndCnt {
   _id: Types.ObjectId;
   count: number;
 }
 
-const findBestDummy = async (userId?: Types.ObjectId) => {
-  const bestCards = await BestCard.find();
-  const cardResList: Nullable<CardResponseDto>[] = await Promise.all(
-    bestCards.map(async bestCard => {
-      const c = await Card.findById(bestCard.card);
-      if (!c) return null;
-      const isBookmarked = userId
-        ? await Bookmark.exists({ user: userId, card: c._id })
-        : false;
-      return {
-        _id: c._id,
-        content: c.content,
-        tags: c.tags,
-        category: c.category,
-        filter: c.filter,
-        isBookmark: !!isBookmarked
-      };
-    })
-  );
-  return cardResList.filter(value => value !== null);
+const findBookmark = async (
+  cardId: Types.ObjectId,
+  userId?: Types.ObjectId
+) => {
+  if (!userId) {
+    return false;
+  }
+  return (await Bookmark.exists({ user: userId, card: cardId })) !== null;
 };
+
+const createCardResponse = async (
+  card: CardDocument,
+  userId: Types.ObjectId | undefined
+): Promise<CardResponseDto> => {
+  return {
+    _id: card._id,
+    content: card.content,
+    tags: card.tags,
+    category: card.category,
+    filter: card.filter,
+    isBookmark: await findBookmark(card._id, userId)
+  };
+};
+
+function notEmpty<CardDocument>(
+  value: CardDocument | null | undefined
+): value is CardDocument {
+  if (value === null || value === undefined) return false;
+  const testDummy: CardDocument = value;
+  return true;
+}
 
 const findBestCards = async (size: number, userId?: Types.ObjectId) => {
-  const cardIdAndCnt = <CardIdAndCnt[]>await Bookmark.aggregate([
-    { $match: { createdAt: { $gte: util.getLastMonth() } } }
-  ])
-    .sortByCount('card')
-    .limit(size);
-  let cards: CardDocument[] = [];
-  let cardList: CardResponseDto[] = [];
-  if (cardIdAndCnt.length > 0) {
-    cards = <CardDocument[]>await Promise.all(
-      cardIdAndCnt.map(async c => {
-        return Card.findById(c._id);
-      })
-    );
-    cardList = await Promise.all(
-      cards.map(async (item: any) => {
-        const isBookmark =
-          (await Bookmark.find({ user: userId, card: item._id }).count()) > 0;
-        return {
-          _id: item._id,
-          content: item.content,
-          tags: item.tags,
-          category: item.Category,
-          filter: item.filter,
-          isBookmark: isBookmark
-        };
-      })
-    );
-  }
-
-  let extraCard: CardResponseDto[] = [];
-  if (cards.length < size) {
-    extraCard = await Card.find({ _id: { $nin: cards.map(c => c._id) } }).limit(
-      size - cards.length
-    );
-  }
-
-  const extraCardList = await Promise.all(
-    extraCard.map(async (item: any) => {
-      const isBookmark =
-        (await Bookmark.find({ user: userId, card: item._id }).count()) > 0
-          ? true
-          : false;
-      return {
-        _id: item._id,
-        content: item.content,
-        tags: item.tags,
-        category: item.Category,
-        filter: item.filter,
-        isBookmark: isBookmark
-      };
-    })
+  const cardsWithBookmarkCount = <CardIdAndCnt[]>(
+    await Bookmark.aggregate().sortByCount('card').limit(size)
   );
-  return [...cardList, ...extraCardList];
+  const cards: CardDocument[] = (
+    await Promise.all(
+      cardsWithBookmarkCount.map(cardWithBookmarkCount =>
+        Card.findById(cardWithBookmarkCount._id)
+      )
+    )
+  ).filter(notEmpty);
+
+  const extraCards: CardDocument[] = await Card.find({
+    _id: { $nin: cards.map(c => c._id) }
+  }).limit(size - cards.length);
+
+  const totalCards: CardResponseDto[] = [];
+  for (const card of [...cards, ...extraCards]) {
+    totalCards.push(await createCardResponse(card, userId));
+  }
+
+  return totalCards;
 };
-export { findBestCards, findBestDummy };
+
+export { findBestCards };
