fef3be623a900f0e0270797f8e8ef86ae4bf8397
donglee99 <jhnj741@naver.com>
2023-06-25 18:05:19 +0900

    [FEAT] Card Api 라우터 추가

--- src/routes/cardRouter.ts
+++ src/routes/cardRouter.ts
@@ -3,7 +3,22 @@ import { CardController } from '../controllers';
 import flexibleAuth from '../middlewares/flexibleAuth';
 
 const router = Router();
-
+router.get(
+  '/cardByBookmarkedGender/:gender',
+  flexibleAuth,
+  CardController.getCardByBookmarkedGender
+);
+router.get(
+  '/recentlyBookmarkedCard',
+  flexibleAuth,
+  CardController.getRecentlyBookmarkedCard
+);
+router.get(
+  '/recentlyUpdatedCard',
+  flexibleAuth,
+  CardController.getRecentlyUpdatedCard
+);
 router.get('/best', flexibleAuth, CardController.getBestCardList);
+router.get('/:cardId', flexibleAuth, CardController.getCards);
 
 export default router;
