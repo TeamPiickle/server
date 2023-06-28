827e9491f46e1f6e8d1b64de166d89a98de3ca0f
kyY00n <dev.kayoung@gmail.com>
2023-06-28 23:18:09 +0900

    [FEAT] api 라우팅

--- src/modules/responseMessage.ts
+++ src/modules/responseMessage.ts
@@ -25,6 +25,8 @@ const message = {
   SEARCH_CARDS_SUCCESS: '필터에 해당하는 카드들 읽어오기 성공',
   READ_CARD_SUCCESS: '카테고리에 해당 카드 읽어오기 성공',
   READ_BEST_CARDS_SUCCESS: '베스트 카드 읽어오기 성공',
+  BLOCK_CARD_SUCCESS: '카드 블랙리스트 추가 성공',
+  CANCEL_BLOCK_CARD_SUCCESS: '카드 블랙리스트 제외 성공',
 
   //카드 메들리
   READ_CARD_MEDLEY_DETAIL_SUCCESS: '카드 메들리 상세 조회 성공',
