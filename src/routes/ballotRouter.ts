27a3b3e06eb8c7a79229878a0cc28b2da279fa7e
Rosie <dev.kayoung@gmail.com>
2023-08-20 20:38:39 +0900

    fix: 투표 주제 조회 제외

--- src/routes/ballotRouter.ts
+++ src/routes/ballotRouter.ts
@@ -2,43 +2,14 @@ import { Router, RequestHandler } from 'express';
 import { body } from 'express-validator';
 import { BallotController } from '../controllers';
 import auth from '../middlewares/auth';
-import flexibleAuth from '../middlewares/flexibleAuth';
-import session from 'express-session';
-import uidSetter from '../middlewares/session/uidSetter';
-import MongoStore from 'connect-mongo';
-import config from '../config';
-import cors from 'cors';
 
 const router = Router();
 
-const expressSession: RequestHandler = session({
-  secret: config.sessionKey,
-  store: MongoStore.create({
-    mongoUrl: config.mongoURI
-  }),
-  cookie: {
-    sameSite: 'none',
-    secure: true,
-    maxAge: 3.6e6 * 24 * 180
-  },
-  resave: false,
-  saveUninitialized: false
-});
-
-router.use(expressSession, uidSetter);
-
-const cookieCors = cors({
-  origin: 'https://client-wheat-three.vercel.app',
-  credentials: true
-});
-
 router.post(
   '',
-  cookieCors,
   [body('ballotTopicId').notEmpty(), body('ballotItemId').notEmpty()],
   BallotController.postBallotResult
 );
-router.get('/:ballotTopicId', cookieCors, BallotController.getBallotStatus);
-router.get('', BallotController.getMainBallotList);
+router.get('/:ballotTopicId', BallotController.getBallotStatus);
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,17 +3,50 @@ import { Router } from 'express';
 import UserRouter from './userRouter';
 import CategoryRouter from './CategoryRouter';
 import BallotRouter from './ballotRouter';
+import { BallotController } from '../controllers';
 import CardRouter from './cardRouter';
 import TestRouter from './testRouter';
 import CardMedleyRouter from './cardMedleyRouter';
 import flexibleAuth from '../middlewares/flexibleAuth';
-import guestHandler from '../middlewares/session/guestHandler';
+
+import session from 'express-session';
+import uidSetter from '../middlewares/session/uidSetter';
+import MongoStore from 'connect-mongo';
+import config from '../config';
+import cors from 'cors';
 
 const router = Router();
 
+const expressSession: RequestHandler = session({
+  secret: config.sessionKey,
+  store: MongoStore.create({
+    mongoUrl: config.mongoURI
+  }),
+  cookie: {
+    sameSite: 'none',
+    secure: true,
+    maxAge: 3.6e6 * 24 * 180
+  },
+  resave: false,
+  saveUninitialized: false
+});
+
+const cookieCors = cors({
+  origin: 'https://client-wheat-three.vercel.app',
+  credentials: true
+});
+
 router.use('/users', UserRouter);
 router.use('/categories', CategoryRouter);
-router.use('/ballots', flexibleAuth, guestHandler, BallotRouter);
+router.get('/ballots', flexibleAuth, BallotController.getMainBallotList);
+router.use(
+  '/ballots',
+  cookieCors,
+  expressSession,
+  uidSetter,
+  flexibleAuth,
+  BallotRouter
+);
 router.use('/cards', CardRouter);
 router.use('/test', TestRouter);
 router.use('/medley', CardMedleyRouter);
