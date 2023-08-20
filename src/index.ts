cdee5a48e8dba10eddf870760e6ec2cf7b2bd055
Rosie <dev.kayoung@gmail.com>
2023-08-20 20:05:00 +0900

    gogo

--- src/index.ts
+++ src/index.ts
@@ -10,32 +10,11 @@ import MongoStore from 'connect-mongo';
 import uidSetter from './middlewares/session/uidSetter';
 
 const app = express();
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-  })
-);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(helmet());
 
-const expressSession: express.RequestHandler = session({
-  secret: config.sessionKey,
-  store: MongoStore.create({
-    mongoUrl: config.mongoURI
-  }),
-  cookie: {
-    // sameSite: 'lax',
-    // httpOnly: config.nodeEnv === 'production',
-    secure: config.nodeEnv === 'production',
-    maxAge: 3.6e6 * 24 * 180
-  },
-  resave: false,
-  saveUninitialized: false
-});
-
-app.use(expressSession, uidSetter);
 app.use(routes);
 app.use(errorHandler);
 
--- src/routes/ballotRouter.ts
+++ src/routes/ballotRouter.ts
@@ -4,13 +4,35 @@ import { BallotController } from '../controllers';
 
 const router = Router();
 
+const expressSession: express.RequestHandler = session({
+  secret: config.sessionKey,
+  store: MongoStore.create({
+    mongoUrl: config.mongoURI
+  }),
+  cookie: {
+    sameSite: 'none',
+    secure: true,
+    maxAge: 3.6e6 * 24 * 180
+  },
+  resave: false,
+  saveUninitialized: false
+});
+
+router.use(expressSession, uidSetter);
+
+const cookieCors = cors({
+  origin: 'https://client-wheat-three.vercel.app',
+  credentials: true
+});
+
 router.post(
   '',
+  cookieCors,
   [body('ballotTopicId').notEmpty(), body('ballotItemId').notEmpty()],
   BallotController.postBallotResult
 );
 
-router.get('/:ballotTopicId', BallotController.getBallotStatus);
+router.get('/:ballotTopicId', cookieCors, BallotController.getBallotStatus);
 router.get('', BallotController.getMainBallotList);
 
 export default router;
