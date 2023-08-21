74c5bbdde91f6552fed1a547fced5be49ce027ac
Rosie <dev.kayoung@gmail.com>
2023-08-22 03:03:40 +0900

    session 설정 app에서 함

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-import express from 'express';
+import express, { RequestHandler } from 'express';
 import connectDB from './loaders/db';
 import routes from './routes';
 import helmet from 'helmet';
-import config from './config';
 import errorHandler from './middlewares/errorHandler';
 import * as SentryConfig from './loaders/sentryConfiguration';
 import corsMiddleware from './middlewares/cors';
+import session from 'express-session';
+import MongoStore from 'connect-mongo';
+import config from './config';
+
+const expressSession: RequestHandler = session({
+  secret: config.sessionKey,
+  store: MongoStore.create({
+    mongoUrl: config.mongoURI
+  }),
+  cookie: {
+    sameSite: 'none',
+    httpOnly: true,
+    secure: true,
+    maxAge: 3.6e6 * 24 * 180
+  },
+  resave: false,
+  saveUninitialized: false
+});
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +30,7 @@ app.use(express.json());
 app.use(helmet());
 SentryConfig.initializeSentry(app);
 
+app.use(expressSession);
 app.use(corsMiddleware);
 app.use(routes);
 SentryConfig.attachSentryErrorHandler(app);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,5 @@
 //router index file
-import { Router, RequestHandler } from 'express';
+import { Router } from 'express';
 import UserRouter from './userRouter';
 import CategoryRouter from './CategoryRouter';
 import BallotRouter from './ballotRouter';
@@ -10,39 +10,13 @@ import CardMedleyRouter from './cardMedleyRouter';
 import flexibleAuth from '../middlewares/flexibleAuth';
 import guestHandler from '../middlewares/session/guestHandler';
 
-import session from 'express-session';
 import uidSetter from '../middlewares/session/uidSetter';
-import MongoStore from 'connect-mongo';
-import config from '../config';
-
 const router = Router();
-
-const expressSession: RequestHandler = session({
-  secret: config.sessionKey,
-  store: MongoStore.create({
-    mongoUrl: config.mongoURI
-  }),
-  cookie: {
-    sameSite: 'none',
-    httpOnly: true,
-    secure: true,
-    maxAge: 3.6e6 * 24 * 180
-  },
-  resave: false,
-  saveUninitialized: false
-});
 
 router.use('/users', UserRouter);
 router.use('/categories', CategoryRouter);
 
-router.use(
-  '/ballots',
-  expressSession,
-  uidSetter,
-  guestHandler,
-  flexibleAuth,
-  BallotRouter
-);
+router.use('/ballots', uidSetter, guestHandler, flexibleAuth, BallotRouter);
 
 router.use('/cards', CardRouter);
 router.use('/test', TestRouter);
