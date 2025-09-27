b1bf9b39bbb49c84275850fcd93b0b214b8fabe1
Kayoung Yoon <dev.kayoung@gmail.com>
2025-09-28 01:19:40 +0200

    [CHORE] migrated aws sdk from v2 to v3

--- src/config/s3Client.ts
+++ src/config/s3Client.ts
@@ -1,9 +1,12 @@
-import AWS from 'aws-sdk';
+import { S3Client } from '@aws-sdk/client-s3';
 import config from '.';
 
-const s3: AWS.S3 = new AWS.S3({
-  accessKeyId: config.s3AccessKey,
-  secretAccessKey: config.s3SecretKey,
+const s3: S3Client = new S3Client({
+  credentials: {
+    accessKeyId: config.s3AccessKey,
+    secretAccessKey: config.s3SecretKey
+  },
+
   region: 'ap-northeast-2'
 });
 
